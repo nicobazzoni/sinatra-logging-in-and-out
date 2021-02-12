7f5d4cac1f83571fab905be0f571ecae4d6e9dae
Nico Bazzoni <you@example.com>
2021-02-12 15:28:05 -0500

    commit tree d91760cf18dae8a74647ea30d03d8dc4d7bdaf0f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,22 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password ==params[:password]
+      session[:user_id] = @user.id 
+      redirect to '/account'
+    end
+    erb:error
 
   end
 
   get '/account' do
+    @using_now = User.find_by_id(session[:user_id])
+    if @using_now
+      erb :account
+    else
+      erb :error
+    end
 
   end
 
